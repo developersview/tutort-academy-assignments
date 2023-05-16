1f2ed64d58f4318794910f577d39a73ec4c82c46
developersview <pcslg1998@gmail.com>
2023-05-16 11:48:08 +0530

    ques 4

---
+++ warmup-assignment-part2/QuestionFourSolution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 16/05/2023
+ */
+public class QuestionFourSolution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(runningSum(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(runningSum(new int[]{1, 1, 1, 1, 1})));
+        System.out.println(Arrays.toString(runningSum(new int[]{3, 1, 2, 10, 1})));
+    }
+
+    private static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        return nums;
+    }
+}
