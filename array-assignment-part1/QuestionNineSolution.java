026c17ec76578147b3f8655ed73163dd001958ea
developersview <pcslg1998@gmail.com>
2023-05-23 23:00:40 +0530

    array assignment question 9 solution

---
+++ array-assignment-part1/QuestionNineSolution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 23/05/2023
+ */
+public class QuestionNineSolution {
+    public static void main(String[] args) {
+        System.out.println(findMejorityElements(new int[]{3, 2, 3}));
+    }
+
+    static List<Integer> findMejorityElements(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                int count = map.get(num) + 1;
+                map.put(num, count);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int value = entry.getValue();
+            int key = entry.getKey();
+            if (value > nums.length / 3) {
+                list.add(key);
+            }
+        }
+        return list;
+    }
+}
