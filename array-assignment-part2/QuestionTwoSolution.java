a938303158416e52052665b72e1dfbbd9035132e
developersview <pcslg1998@gmail.com>
2023-05-29 19:26:11 +0530

    array question 2 solution

---
+++ array-assignment-part2/QuestionTwoSolution.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+/**
+ * @author pranoy.chakraborty
+ * @Date 29/05/2023
+ */
+public class QuestionTwoSolution {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 4}));
+        System.out.println(containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    }
+
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
