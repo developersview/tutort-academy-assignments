e1777d002fbde1c12f2f3942de91e9086e1675e6
developersview <pcslg1998@gmail.com>
2023-06-07 14:34:24 +0530

    assign cookies

---
+++ array-assignment-part3/AssignCookies.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 07/06/2023
+ */
+public class AssignCookies {
+    public static void main(String[] args) {
+        System.out.println(findContentChildren(new int[]{1, 2, 3}, new int[]{1, 2}));
+    }
+
+    static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int contentChildren = 0;
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                contentChildren++;
+                i++;
+            }
+            j++;
+        }
+        return contentChildren;
+    }
+}
