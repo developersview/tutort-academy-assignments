3e1a0d42b2bfd37ec55ef616d4cb439be48f471c
developersview <pcslg1998@gmail.com>
2023-06-07 14:33:46 +0530

    disapperaed number

---
+++ array-assignment-part3/DisappearedNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 07/06/2023
+ */
+public class DisappearedNumber {
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumber(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+    }
+
+    static List<Integer> findDisappearedNumber(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (!numSet.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
