33cad607fa7b39b1a1139e9515932fa8c8a7ae2c
developersview <pcslg1998@gmail.com>
2023-06-11 21:18:26 +0530

    power of n

---
+++ recursion-assignment/PowerOfN.java
@@ -0,0 +1,28 @@
+/**
+ * @author pranoy.chakraborty
+ * @Date 11/06/2023
+ */
+public class PowerOfN {
+    public static void main(String[] args) {
+        System.out.println(myPow(2.0000, 10));
+        System.out.println(myPow(2.000,-2));
+    }
+
+    static double myPow(double x, int n) {
+        double temp;
+        if (n == 0) return 1;
+        temp = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else if (n > 0) {
+            return x * temp * temp;
+        } else {
+            return temp * temp / x;
+        }
+        /*if (n == 0) {
+            return 1;
+        }
+        return x * myPow(x, n - 1);*/
+    }
+
+}
