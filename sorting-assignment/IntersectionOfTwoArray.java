c11fada827afb1bfe87f0572bfede461201c8e5a
developersview <pcslg1998@gmail.com>
2023-06-28 19:13:05 +0530

    formatted code

--- sorting-assignment/IntersectionOfTwoArray.java
+++ sorting-assignment/IntersectionOfTwoArray.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class IntersectionOfTwoArray {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(intersection(new int[]{1,2,2,1},new int[]{2,2})));
+        System.out.println(Arrays.toString(intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
     }
 
     static int[] intersection(int[] nums1, int[] nums2) {
